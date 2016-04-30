843c615fe396bbd9786f3da51dd56a934f49d22d
Jimmie Elvenmark <flugsio@gmail.com>
2016-04-30 19:00:09 +0200

    clean code + tests, handle 0 entries case

--- src/graph.rs
+++ src/graph.rs
@@ -10,7 +10,7 @@ pub struct Entry {
 }
 
 pub struct View {
-    latest_time: DateTime<UTC>,
+    latest: Option<DateTime<UTC>>,
     entries: HashMap<DateTime<UTC>, Vec<Entry>>,
 }
 
@@ -25,16 +25,14 @@ enum Show {
 impl fmt::Display for Show {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            &Show::Empty      => write!(f, "{}", " "),
-            &Show::Timeline   => write!(f, "{}", "|"),
-            &Show::Waitline   => write!(f, "{}", "-"),
-            &Show::Glucose(x) if x < 0 || x > 33
-                              => write!(f, "?"),
-            &Show::Glucose(x) => {
-                // cleaner version sadly only handles 0-15: write!(f, "{:X}", x)
+            &Show::Empty      => write!(f, " "),
+            &Show::Timeline   => write!(f, "|"),
+            &Show::Waitline   => write!(f, "-"),
+            &Show::Glucose(x) if -1 < x && x < 34 => {
                 let y = char::from_digit(x as u32, 36).unwrap().to_uppercase().next().unwrap();
                 write!(f, "{}", y)
             },
+            &Show::Glucose(_) => write!(f, "?"),
         }
     }
 }
@@ -42,14 +40,14 @@ impl fmt::Display for Show {
 impl View {
     pub fn new(entries: Vec<Entry>) -> View {
         let mut grouped_entries = HashMap::new();
-        let latest_time = entries.last().unwrap().at.clone();
+        let latest = entries.last().map(|e| e.at.clone());
         for entry in entries {
             let rounded = floor_time(entry.at);
             let list = grouped_entries.entry(rounded).or_insert(Vec::new());
             list.push(entry);
         }
         View {
-            latest_time: latest_time,
+            latest: latest,
             entries: grouped_entries,
         }
     }
@@ -69,8 +67,10 @@ impl View {
         match self.entries.get(&time) {
             Some(entry) => Show::Glucose(entry.last().unwrap().glucose),
             None if even_full_hour(time) => Show::Timeline,
-            None if (self.latest_time < time) => Show::Waitline,
-            None => Show::Empty,
+            None => match self.latest {
+                Some(latest) if (time < latest) => Show::Empty,
+                _ => Show::Waitline,
+            },
         }
     }
 
@@ -153,6 +153,6 @@ mod tests {
         let entry_date = UTC.ymd(2015, 1, 15).and_hms(11, 5, 30);
         let show_date  = UTC.ymd(2015, 1, 15).and_hms(12, 0,  0);
         let entries = vec!(Entry { at: entry_date, glucose: 7 } );
-        assert_eq!("-------|-------|-------|---7---|".to_string(), View::new(entries).render(show_date));
+        assert_eq!("       |       |       |   7---|".to_string(), View::new(entries).render(show_date));
     }
 }
