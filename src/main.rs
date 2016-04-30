7c5a559db0fd53f9014f1364334f62951a1ef288
Jimmie Elvenmark <flugsio@gmail.com>
2016-04-30 21:00:36 +0200

    xdg data location for entries

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate chrono;
 extern crate csv;
+extern crate xdg;
 
 use std::io::prelude::*;
 use std::fs::File;
@@ -9,13 +10,16 @@ mod graph;
 
 #[allow(dead_code)]
 fn main() {
-    let entries = load_entries("/home/flugsio/.glucose");
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("gluj").unwrap();
+    let glucose_path = xdg_dirs.find_data_file("glucose.csv")
+        .expect("Need file: $XDG_DATA_HOME/gluj/glucose.csv");
+    let entries = load_entries(glucose_path);
     let now = UTC::now();
     let graph = graph::View::new(entries);
     println!("{}", graph.render(now));
 }
 
-fn load_entries(path: &str) -> Vec<graph::Entry> {
+fn load_entries(path: std::path::PathBuf) -> Vec<graph::Entry> {
     let mut f = File::open(path).unwrap();
     let mut data = String::new();
     f.read_to_string(&mut data).unwrap();
